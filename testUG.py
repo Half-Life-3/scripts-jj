54ed8e77d695994366d7509ff85a9c3e10430a7d
Joel <joel.joshi@smoothstack.com>
2022-04-12 19:29:00 -0400

    testing and documentation

--- testUG.py
+++ testUG.py
@@ -1,4 +1,23 @@
 import userGenerator as ug
+import sendApps as app
+import unittest
 
+#print(ug.genUsers(None))
 
-print(ug.genUsers(None))
+class TestUserGeneration(unittest.TestCase):
+	
+	def test_createAUser(self): #whether a generated user name is 2 strings
+		aName = ug.genUsers(1)[0]
+		self.assertEqual(type(aName[0]),str)
+		self.assertEqual(type(aName[1]),str)
+		
+		
+		
+class TestAppGeneration(unittest.TestCase):
+	def test_sendAnApp(self): #whether a generated app results in a sucess state
+		self.assertEqual(app.sendApp()[0],201)
+		
+		
+		
+if __name__=="__main__":
+	unittest.main()
